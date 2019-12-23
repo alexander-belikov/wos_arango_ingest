5fe4239fa1ee65df998d40ebc4fd2ac4b939653b
Alexander Belikov <abelikov@gmail.com>
2019-12-23 14:13:50 +0100

    upd q1,3,4

--- examples/arango/query1.py
+++ examples/arango/query1.py
@@ -1,19 +1,39 @@
+import numpy as np
+from arango import ArangoClient
 from wos_db_studies.utils import profile_query
 
-n_profile = 3
-nq = 1
+q_aux = """
+FOR p IN publications
+    LET contrs = (FOR c IN 1..1 INBOUND p contributors_publications_edges RETURN c)
+    LET orgs = (FOR org IN 1..1 INBOUND p organizations_publications_edges RETURN org)
+    FOR c in contrs
+        FOR org in orgs
+            INSERT {_from : c._id, _to : org._id, "wosid": p._key, "year": p.year} 
+            IN contributors_organizations_edges
+"""
 
 test = False
-test = True
+# test = True
+nq = 4
 
-if test:
-    limit_str = 'LIMIT 1000'
-else:
-    limit_str = ''
+port = 8529
+ip_addr = '127.0.0.1'
+cred_name = 'root'
+cred_pass = '123'
+hosts = f'http://{ip_addr}:{port}'
 
-q = f"""
+client = ArangoClient(hosts=hosts)
+sys_db = client.db('_system', username=cred_name, password=cred_pass)
+
+cname = 'media'
+
+r = sys_db.aql.execute(f'RETURN LENGTH({cname})')
+n = list(r)[0]
+order_max = int(np.log(n)/np.log(10))
+
+q0 = f"""
 LET cnts =
-    (FOR j IN media
+    (FOR j IN media _insert_limit
         LET cc = (FOR v IN 1..1 INBOUND j publications_media_edges
             FILTER v.year == 1978
             RETURN v)
@@ -23,7 +43,26 @@ FOR doc in cnts
     LIMIT 10
 RETURN doc"""
 
-fpath = './../../results/'
+n_profile = 3
+
+orders = np.arange(1, order_max, 1)
+limits = 10 ** orders
+if test:
+    limits = [100, 1000]
+else:
+    limits = [int(n) for n in limits] + [None]
+
+print(limits)
+for limit in limits:
+    fpath = './../../results/'
+    q = q0.replace('_insert_limit', f'LIMIT {limit}')
+    print(q)
+    profile_query(q, nq, n_profile, fpath, limit)
 
 
-profile_query(q, nq, n_profile, fpath)
+
+
+
+
+
+
--- examples/arango/query4.py
+++ examples/arango/query4.py
@@ -1,26 +1,50 @@
+import numpy as np
+from arango import ArangoClient
 from wos_db_studies.utils import profile_query
-
-n_profile = 3
-nq = 4
 
 test = False
 test = True
+nq = 4
 
-if test:
-    limit_str = 'LIMIT 1000'
-else:
-    limit_str = ''
+port = 8529
+ip_addr = '127.0.0.1'
+cred_name = 'root'
+cred_pass = '123'
+hosts = f'http://{ip_addr}:{port}'
 
-q = f"""
+client = ArangoClient(hosts=hosts)
+sys_db = client.db('_system', username=cred_name, password=cred_pass)
+
+
+cname = 'publications'
+
+r = sys_db.aql.execute(f'RETURN LENGTH({cname})')
+n = list(r)[0]
+order_max = int(np.log(n)/np.log(10))
+
+q0 = f"""
 LET cnts = (
-    FOR p IN publications {limit_str}
+    FOR p IN publications _insert_limit
         LET first = (FOR c IN 1..1 INBOUND p publications_publications_edges RETURN c._id)
         LET second = (FOR c IN 2..2 INBOUND p publications_publications_edges RETURN DISTINCT c._id)
         RETURN {{pub: p._id, fa: LENGTH(first), fb: LENGTH(second)}}
         )
      return cnts"""
 
-fpath = './../../results/'
 
+n_profile = 3
 
-profile_query(q, nq, n_profile, fpath)
+orders = np.arange(1, order_max, 1)
+limits = 10 ** orders
+if test:
+    limits = [100, 1000]
+else:
+    limits = [int(n) for n in limits] + [None]
+
+print(limits)
+for limit in limits:
+    fpath = './../../results/'
+    q = q0.replace('_insert_limit', f'LIMIT {limit}')
+    print(q)
+    profile_query(q, nq, n_profile, fpath, limit)
+
