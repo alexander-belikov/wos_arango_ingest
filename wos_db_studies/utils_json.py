3bf4865f8636c2c45d7c4b2008eef0da905028f5
Alexander Belikov <abelikov@gmail.com>
2020-12-27 18:46:09 +0100

    version 0.0.-1

--- wos_db_studies/utils_json.py
+++ wos_db_studies/utils_json.py
@@ -7,26 +7,29 @@ def apply_mapper(mapper, document, vertex_indices):
     if "how" in mapper:
         mode = mapper["how"]
         vcol = mapper["name"]
+        if "__extra" in mapper:
+            extra_dict = mapper["__extra"]
+        else:
+            extra_dict = {}
         # value is a dict
         if mode == "dict":
             field_map = mapper["map"]
             return {
                 vcol: [
                     {
-                        **{v: document[k] for k, v in field_map.items() if k in document},
+                        **{
+                            v: document[k]
+                            for k, v in field_map.items()
+                            if k in document
+                        },
+                        **extra_dict
                         # **{k: v for k, v in document.items() if k not in field_map},
                     }
                 ]
             }
         elif mode == "value":
             key = mapper["key"]
-            return {
-                vcol: [
-                    {
-                        **{key: document}
-                    }
-                ]
-            }
+            return {vcol: [{**{key: document}}]}
         else:
             raise KeyError("Mapper must have map key if it has vertex key")
     # traverse non terminal nodes
@@ -62,41 +65,92 @@ def apply_mapper(mapper, document, vertex_indices):
 
 def add_edges(mapper, agg, vertex_indices):
     for edge_def in mapper["edges"]:
+        # get source and target names
         source, target = (
             edge_def["source"]["name"],
             edge_def["target"]["name"],
         )
+        # get source and target edge fields
+        source_index, target_index = vertex_indices[source], vertex_indices[target]
 
-        source_index, target_index = vertex_indices[source], vertex_indices[target],
-        source_items = agg[source]
-        target_items = agg[target]
+        # if "fields" in edge_def["source"]:
+        #     source_index += edge_def["source"]["fields"]
+        # if "fields" in edge_def["target"]:
+        #     target_index += edge_def["target"]["fields"]
+        # get source and target items
+        source_items, target_items = agg[source], agg[target]
+
         if edge_def["how"] == "all":
-            if "ufield" in edge_def["source"]:
-                source_items = [item for item in source_items if edge_def["source"]["ufield"] in item]
-            if "ufield" in edge_def["target"]:
-                target_items = [item for item in target_items if edge_def["target"]["ufield"] in item]
-            source_items = project_dict(source_items, source_index)
-            target_items = project_dict(target_items, target_index)
+            source_items = pick_indexed_items_anchor_logic(source_items,
+                                                           source_index,
+                                                           edge_def["source"])
+            target_items = pick_indexed_items_anchor_logic(target_items,
+                                                           target_index,
+                                                           edge_def["target"])
             for u, v in product(source_items, target_items):
-                agg[(source, target)] += [(u, v)]
+                weight = dict()
+                if "fields" in edge_def["source"]:
+                    weight.update({k: u[k] for k in edge_def["source"]["fields"] if k in u})
+                if "fields" in edge_def["target"]:
+                    weight.update({k: v[k] for k in edge_def["target"]["fields"] if k in v})
+                agg[(source, target)] += [
+                    (
+                        project_dict(u, source_index),
+                        project_dict(v, target_index),
+                        weight,
+                    )
+                ]
         if edge_def["how"] == "1-n":
             source_field, target_field = (
                 edge_def["source"]["field"],
                 edge_def["target"]["field"],
             )
-            targets = dict(zip([item[target_field] for item in target_items],  project_dict(target_items, target_index)))
+            source_items = pick_indexed_items_anchor_logic(source_items,
+                                                           source_index,
+                                                           edge_def["source"])
+            target_items = pick_indexed_items_anchor_logic(target_items,
+                                                           target_index,
+                                                           edge_def["target"])
+            target_items = [item for item in target_items if target_field in item]
+            target_items = dict(
+                zip(
+                    [item[target_field] for item in target_items],
+                    project_dicts(target_items, target_index),
+                )
+            )
             if "all_value" in edge_def["source"]:
                 all_value = edge_def["source"]["all_value"]
             else:
                 all_value = None
             for u in source_items:
                 pointers = u[source_field]
-                up = project_dict([u], source_index)[0]
+                up = project_dict(u, source_index)
+                weight = dict()
                 if all_value is not None and all_value in pointers:
-                    agg[(source, target)] += [(up, v) for v in targets.values()]
+                    agg[(source, target)] += [(up, v, weight) for v in target_items.values()]
                 else:
-                    agg[(source, target)] += [(up, targets[p]) for p in pointers]
+                    agg[(source, target)] += [
+                        (up, target_items[p], weight) for p in pointers
+                    ]
+
     return agg
+
+
+def pick_indexed_items_anchor_logic(items, indices, set_spec):
+    items_ = [
+        item for item in items if any([k in item for k in indices])
+    ]
+
+    if "anchor" in set_spec:
+        if set_spec["anchor"]:
+            items_ = [
+                item for item in items_ if "anchor" in item and item["anchor"]
+            ]
+        else:
+            items_ = [
+                item for item in items_ if "anchor" not in item
+            ]
+    return items_
 
 
 def assign_edge_label(edges, label, condition):
@@ -108,7 +162,14 @@ def clean_arobas(item):
     return {k: v for k, v in item.items() if k[0] != "@"}
 
 
-def project_dict(items, keys, how="include"):
+def project_dict(item, keys, how="include"):
+    if how == "include":
+        return {k: v for k, v in item.items() if k in keys}
+    elif how == "exclude":
+        return {k: v for k, v in item.items() if k not in keys}
+
+
+def project_dicts(items, keys, how="include"):
     if how == "include":
         return [{k: v for k, v in item.items() if k in keys} for item in items]
     elif how == "exclude":
@@ -121,7 +182,9 @@ def clean_aux_fields(pack):
         if k != "@edges":
             pack_out[k] = [clean_arobas(x) for x in cpack]
         else:
-            pack_out[k] = [(clean_arobas(x[0]), clean_arobas(x[1]), x[2:]) for x in cpack]
+            pack_out[k] = [
+                (clean_arobas(x[0]), clean_arobas(x[1]), x[2:]) for x in cpack
+            ]
     return pack_out
 
 
@@ -156,18 +219,28 @@ def parse_edges(croot, edge_acc, mapping_fields):
             return [], defaultdict(list)
 
 
-def merge_documents(docs):
-    return [dict(ChainMap(*docs))]
+def merge_documents(docs, key_absent_aux="_key", anchor_key="anchor"):
+    mains_, mains, auxs, anchors = [], [], [], []
+    for item in docs:
+        (mains_ if key_absent_aux in item else auxs).append(item)
+
+    for item in mains_:
+        (anchors if anchor_key in item and item[anchor_key] else mains).append(item)
+
+    auxs = auxs + anchors
+    r = [dict(ChainMap(*auxs))] + mains
+    return r
 
 
-def process_document_top(config, doc, index_fields_dict, edge_fields, merge_collections):
+def process_document_top(
+    config, doc, index_fields_dict, edge_fields, merge_collections
+):
     acc = apply_mapper(config, doc, index_fields_dict)
     for k, v in acc.items():
         v = pick_unique_dict(v)
         if not isinstance(k, tuple):
-            v = project_dict(v, edge_fields[k], how="exclude")
+            v = project_dicts(v, edge_fields[k], how="exclude")
         if k in merge_collections:
             v = merge_documents(v)
         acc[k] = v
     return acc
-
